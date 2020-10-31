c57776b4277830ce0d4cbeb1035da4adf3030ef4
Angus Walker <angusw@diabeticappsolutions.com>
2020-10-31 07:31:53 -0400

    Sat AM psuh for 22 minutes.

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RPG.Controllers
+{
+    [ApiController]
+    [Route("controller")]
+    public class CharacterController : ControllerBase
+    {
+        private static  knight" = new Character();
+    }
+}
---
+++ Models/RpgClass.cs
@@ -0,0 +1,12 @@
+namespace RPG.Models
+{
+    public enum RpgClass
+    {
+        Knight = 1,
+        Mage = 2,
+        Cleric = 3,
+        Healer = 4,
+        Wizard = 5
+
+    }
+}
